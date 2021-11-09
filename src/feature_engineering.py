d37a394ee8ce146bcf2e1e60b49ad97b26940de0
Bakar31 <abubakar1808031@gmail.com>
2021-11-09 13:13:50 +0600

    new features

---
+++ src/feature_engineering.py
@@ -0,0 +1,47 @@
+from input import train, test
+
+def add_features(df):
+    df['area'] = df['time_step'] * df['u_in']
+    df['area1'] = df.groupby('breath_id')['area'].cumsum()
+    df['u_in_cumsum'] = (df['u_in']).groupby(df['breath_id']).cumsum()
+    df['u_in_lag1'] = df.groupby('breath_id')['u_in'].shift(1)
+    df['u_out_lag1'] = df.groupby('breath_id')['u_out'].shift(1)
+    df['u_in_lag_back1'] = df.groupby('breath_id')['u_in'].shift(-1)
+    df['u_out_lag_back1'] = df.groupby('breath_id')['u_out'].shift(-1)
+    df['u_in_lag2'] = df.groupby('breath_id')['u_in'].shift(2)
+    df['u_out_lag2'] = df.groupby('breath_id')['u_out'].shift(2)
+    df['u_in_lag_back2'] = df.groupby('breath_id')['u_in'].shift(-2)
+    df['u_out_lag_back2'] = df.groupby('breath_id')['u_out'].shift(-2)
+    df['u_in_lag3'] = df.groupby('breath_id')['u_in'].shift(3)
+    df['u_out_lag3'] = df.groupby('breath_id')['u_out'].shift(3)
+    df['u_in_lag_back3'] = df.groupby('breath_id')['u_in'].shift(-3)
+    df['u_out_lag_back3'] = df.groupby('breath_id')['u_out'].shift(-3)
+    df['u_in_lag4'] = df.groupby('breath_id')['u_in'].shift(4)
+    df['u_out_lag4'] = df.groupby('breath_id')['u_out'].shift(4)
+    df['u_in_lag_back4'] = df.groupby('breath_id')['u_in'].shift(-4)
+    df['u_out_lag_back4'] = df.groupby('breath_id')['u_out'].shift(-4)
+    df['breath_id__u_in__max'] = df.groupby(['breath_id'])['u_in'].transform('max')
+    df['breath_id__u_out__max'] = df.groupby(['breath_id'])['u_out'].transform('max')
+    df['breath_id__u_in__diffmax'] = df.groupby(['breath_id'])['u_in'].transform('max') - df['u_in']
+    df['breath_id__u_in__diffmean'] = df.groupby(['breath_id'])['u_in'].transform('mean') - df['u_in']
+    df['breath_id__u_in__diffmax'] = df.groupby(['breath_id'])['u_in'].transform('max') - df['u_in']
+    df['breath_id__u_in__diffmean'] = df.groupby(['breath_id'])['u_in'].transform('mean') - df['u_in']
+    df['u_in_diff3'] = df['u_in'] - df['u_in_lag3']
+    df['u_out_diff3'] = df['u_out'] - df['u_out_lag3']
+    df['u_in_diff4'] = df['u_in'] - df['u_in_lag4']
+    df['u_out_diff4'] = df['u_out'] - df['u_out_lag4']
+    df['cross']= df['u_in']*df['u_out']
+    df['cross2']= df['time_step']*df['u_out']
+    df['u_in_diff1'] = df['u_in'] - df['u_in_lag1']
+    df['u_out_diff1'] = df['u_out'] - df['u_out_lag1']
+    df['u_in_diff2']  = df['u_in'] - df['u_in_lag2'] 
+    df['u_out_diff2'] = df['u_out'] - df['u_out_lag2'] 
+    df['u_in_diff3']  = df['u_in'] - df['u_in_lag3'] 
+    df['u_out_diff3'] = df['u_out'] - df['u_out_lag3'] 
+    df['u_in_diff4'] = df['u_in'] - df['u_in_lag4'] 
+    df['u_out_diff4'] = df['u_out'] - df['u_out_lag4'] 
+    df = df.fillna(0)
+    return df
+
+train = add_features(train)
+test = add_features(test)
