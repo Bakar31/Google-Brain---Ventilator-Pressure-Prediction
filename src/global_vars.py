b0505089b2ffd2ae034f257ee47adaaf91ff7772
Bakar31 <abubakar1808031@gmail.com>
2021-11-09 13:16:45 +0600

    global variables

---
+++ src/global_vars.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.callbacks import LearningRateScheduler
+from tensorflow.keras.optimizers.schedules import ExponentialDecay
+
+EPOCH = 100
+BATCH_SIZE = 256
+
+lr = tf.keras.callbacks.ReduceLROnPlateau(monitor="val_loss", factor=0.5, patience=10, verbose=1)
+es = EarlyStopping(monitor="val_loss", patience=10, verbose=1, mode="min", restore_best_weights=True)
