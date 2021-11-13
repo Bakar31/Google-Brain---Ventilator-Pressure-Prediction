418bce97165d5f6ae7c642a532b341d0efeb74bf
Bakar31 <abubakar1808031@gmail.com>
2021-11-13 14:53:45 +0600

    update

--- src/config.py
+++ src/config.py
@@ -1,8 +1,16 @@
+import pandas as pd
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.optimizers.schedules import ExponentialDecay
+
+# datas
+train = pd.read_csv('../input/ventilator-pressure-prediction/train.csv')
+test = pd.read_csv('../input/ventilator-pressure-prediction/test.csv')
+ss = pd.read_csv('../input/ventilator-pressure-prediction/sample_submission.csv')
+
+pd.set_option('display.max_columns', None)
 
 EPOCH = 100
 BATCH_SIZE = 256
--- src/input.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# datas
-train = pd.read_csv('../input/ventilator-pressure-prediction/train.csv')
-test = pd.read_csv('../input/ventilator-pressure-prediction/test.csv')
-ss = pd.read_csv('../input/ventilator-pressure-prediction/sample_submission.csv')
-
-pd.set_option('display.max_columns', None)
